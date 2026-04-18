34f76e0d0c900f721fdfacc0abe039acd5db1d7b
Sefger <sefger2006@gmail.com>
2026-04-18 12:26:21 +0300

    bug

---
+++ index-service/src/main.rs
@@ -0,0 +1,17 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/health", get(|| async { "OK" }));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("Index Service listening on {}", addr);
+
+    axum::serve::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
